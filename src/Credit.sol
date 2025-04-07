3c6b9c003ca820628d9f585d6198471c69d7d27e
12anand34 <anand47bansal#gmail.com>
2025-04-07 16:22:30 +0000

    fixed collateral deposit

--- src/Credit.sol
+++ src/Credit.sol
@@ -21,7 +21,7 @@ import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/shared/interf
  * @notice This project is set in the future where common people uses crypto and it will act as replacement to traditional bank. Usually banks fear that a person might default a loan but this protocol is not scared of that. Instead it is more interested in giving out credit as long as collateral meets the requirement. Actually Liquidation is bad for the protocol in the sense that it is more interested in interest accumulated on the person.
  */
 contract CreditScore is ReentrancyGuard {
-    using UD60x18 for uint256; 
+    using UD60x18 for uint256;
 
     IERC20 public immutable TB;
     address public addressOfB;
@@ -77,9 +77,15 @@ contract CreditScore is ReentrancyGuard {
         _;
     }
 
-    //Need to fix this
     modifier validCollateralToken(address token) {
-        require(token == addressOfA, CS__InvalidCollateralTokenAddress(token));
+        bool isValid = false;
+        for (uint256 i = 0; i < collateralTokens.length; i++) {
+            if (collateralTokens[i] == token) {
+                isValid = true;
+                break;
+            }
+        }
+        require(isValid, CS__InvalidCollateralTokenAddress(token));
         _;
     }
 
@@ -100,8 +106,8 @@ contract CreditScore is ReentrancyGuard {
 
     //Fix this
     function depositCollateral(address token, uint256 amount) public moreThanZero(amount) validCollateralToken(token) {
-        TA.transferFrom(msg.sender, address(this), amount);
-        ADeposited[msg.sender] += amount;
+        IERC20(token).transferFrom(msg.sender, address(this), amount);
+        collateralDeposited[msg.sender][token] += amount;
         emit CS__Collateral_Deposited(token, amount);
     }
 
@@ -138,20 +144,15 @@ contract CreditScore is ReentrancyGuard {
     }
 
     /**
-     * 
+     *
      * @param user The user whose credit score is calculated
      * @notice This function calculates the collateral mix score based on the distribution of collateral tokens
-     * @notice The score is calculated based on the number of different tokens and their proportions. 
+     * @notice The score is calculated based on the number of different tokens and their proportions.
      * @notice Uses Entropy Function to evaluate how good the distribution is. eg: 30 30 30 is good while 90 0 0 is bad
      * @notice Takes account in the value of collateral and not the amount of token.
      * @notice The reason that this is important because the protocol is more interested in interest on debt rather than liquidation fees. By diversifying collateral, risk of getting liquidated is reduced.
      */
-
-    function calculateCollateralMixScore(address user)
-        internal
-        pure
-        returns (uint256)
-    {
+    function calculateCollateralMixScore(address user) internal pure returns (uint256) {
         uint256 tokenCount;
         uint256 totalValue;
         mapping(address token => uint256 amount) userCollateral = collateralDeposited[user];
@@ -176,9 +177,9 @@ contract CreditScore is ReentrancyGuard {
             if (amount > 0) {
                 uint256 tokenValue = getUsdValue(token, amount);
                 // Calculate proportion of each token's value to total value
-                uint256 proportion = (tokenValue*PRECISION).div(totalValue); //For eg: 0.5e18
-                uint256 proportionlog = ((1e18*PRECISION).div(proportion).ln()*PRECISION- 18*(23025e14)); //For eg: ln(1/p e18) ---> x e18
-                entropy += (proportion * proportionlog)/1e18; // xe36
+                uint256 proportion = (tokenValue * PRECISION).div(totalValue); //For eg: 0.5e18
+                uint256 proportionlog = ((1e18 * PRECISION).div(proportion).ln() * PRECISION - 18 * (23025e14)); //For eg: ln(1/p e18) ---> x e18
+                entropy += (proportion * proportionlog) / 1e18; // xe36
             }
         }
 
